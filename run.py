4ff4956fe57ad79f5d4557d42c738c67dc8834b1
SiobhanP1 <siobhanphillips11@gmail.com>
2023-03-05 13:32:03 +0000

    Add user request loop to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+"""module docstring"""
+
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -12,34 +14,41 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
     Collect sales data as input from user.
     """
-    print("Please enter sales data from the most recent market.")
-    print("The data should be six numbers, separated by commas")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the most recent market.")
+        print("The data should be six numbers, separated by commas")
+        print("Example: 10,20,30,40,50,60\n")
+        data_str = input("Enter data here: ")
+        sales_data = data_str.split(",")
+        print(sales_data)
+        if validate_data(sales_data):
+            print("Data is valid")
+            break
 
-    data_str = input("Enter data here: ")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+    return sales_data
+
 
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
-    or if there are not exactly six values. 
+    or if there are not exactly six values.
     """
-    try: 
-        [int(value) for value in values]
+    try:
+        values = [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"You entered {len(values)} numbers. Please enter six."
             )
-    except ValueError as e:
-        print(f"Invalid data:{e} Try again.\n")
+    except ValueError as error:
+        print(f"Invalid data:{error} Try again.\n")
+        return False
+    return True
 
-get_sales_data()
 
-
-
+data = get_sales_data()
